1d2bd92a327b8fb27ed588b440b5bbd699f9c975
JasonRafeMiller <jmill02@shepherd.edu>
2019-11-20 07:57:25 -0800

    Directed and Undirected.

--- Python/Tutorial.py
+++ Python/Tutorial.py
@@ -1,31 +1,46 @@
-# pip3 install python-igraph
-# python3
+#!/usr/local/bin/python3
+# Assume: pip3 install python-igraph
 import igraph
 
-ug = igraph.Graph(4)
-ug.add_edges( [ (0,1), (1,2), (2,3) ] )
-ug["name"] = "Demo Undirected Graph"
+class Tutorial:
 
-print ( ug )
-# print ( dir(ug) )  # show every possible attribute
-# print ( ug["name"] )   # show one attribute
-print ( " degree: " ,end='' )
-print ( ug.degree( [0,1,2,3] ) )
-print ( " edge betweenness: " ,end='' )
-print ( ug.edge_betweenness())
-print ( " node betweenness " ,end='' )
-print (ug.betweenness())
-ug = None
+    def print_basic_stats ( self, graph ):
+        print ( "----------------")
+        print ( graph )
+        # print ( dir(graph) )  # show every possible attribute
+        # print ( graph["name"] )   # show one attribute
+        if graph.is_directed():
+            print ("Directed graph")
+            print ( " in-degree: " ,end='' )
+            print ( graph.indegree( [0,1,2,3] ) )
+            print ( " out-degree: " ,end='' )
+            print ( graph.outdegree( [0,1,2,3] ) )
+            # print ( graph.betweenness(vertices=[0,1,2,3], directed=True) )
+        else:
+            print ("Undirected graph")
+            print ( " degree: " ,end='' )
+            print ( graph.degree( [0,1,2,3] ) )
+        print ( " node betweenness " ,end='' )
+        print ( graph.betweenness() )
+        print ( " edge betweenness: " ,end='' )
+        print ( graph.edge_betweenness() )
+        print ( "----------------")
 
-dg = igraph.Graph(4)
-dg["name"] = "Demo Directed Graph"
-dg.add_edges( [ (0,1), (1,2), (2,3) ] )
+    def main (self):
 
-print ( dg )
-print ( " degree: " ,end='' )
-print ( dg.degree( [0,1,2,3] ) )
-print ( " edge betweenness: " ,end='' )
-print ( dg.edge_betweenness())
-print ( " node betweenness " ,end='' )
-print ( dg.betweenness())
-dg = None
+        ug = igraph.Graph(4)
+        ug.add_edges( [ (0,1), (1,2), (2,3) ] )
+        ug["name"] = "Undirected Graph Tutorial"
+        self.print_basic_stats (ug)
+        un = None
+
+        dg = igraph.Graph(4,directed=True)
+        dg.add_edges( [ (0,1), (1,2), (2,3) ] )
+        dg["name"] = "Directed Graph Tutorial"
+        self.print_basic_stats (dg)
+        dn = None
+
+if __name__ == '__main__':
+    instance=Tutorial()
+    instance.main()
+
